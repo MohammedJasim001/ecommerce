9bf230f214f279c613c4c03aa3a7d86a341faf40
MohammedJasim001 <jasimat001@gmail.com>
2024-08-21 19:48:44 +0530

    first commit

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -1,12 +1,15 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Items } from "../MainPage/Main";
 import SearchResults from "./SearchResults";
 import { FaCartPlus } from "react-icons/fa";
 import { MdLogout, MdVerified, MdAccountCircle } from "react-icons/md";
+import { FaSearch } from "react-icons/fa";
 import axios from "axios";
+import { toast } from "sonner";
 
 const Navbar = () => {
+  const navigate=useNavigate()
   const [users, setUsers] = useState([]);
   const { data } = useContext(Items);
   const [input, setInput] = useState("");
@@ -25,6 +28,9 @@ const Navbar = () => {
   useEffect(() => {
     fetchUserData();
   }, []);
+
+  console.log(users);
+  
 
   const cartCount = users.cart ? Object.keys(users.cart).length : 0;
 
@@ -56,32 +62,37 @@ const Navbar = () => {
     isDrop == false ? setIsDrop(true) : setIsDrop(false);
   };
 
+
   return (
     <div className="h-24 bg-white shadow-md">
       <div className="flex justify-between items-center p-5">
-        <h1 className="text-2xl font-bold text-gray-800">
+        <h1 className=" text-2xl font-bold text-gray-800 pr-3">
           Luna<span className="text-green-500">Pets</span>
         </h1>
 
-        <input
-          className="border border-gray-300 w-full max-w-md h-10 rounded-lg px-4"
-          placeholder="Search Products..."
-          onChange={handleChange}
-          value={input}
-          type="text"
-        />
+        <div className="relative flex items-center w-full max-w-md ">
+          <input
+            className="border border-gray-300 h-10 rounded-lg pl-10 pr-4 w-full" // Padding left added for space before the icon
+            placeholder="Search Products..."
+            onChange={handleChange}
+            value={input}
+            type="text"
+          />
+          <FaSearch className="absolute left-3 text-gray-500" />
+         
+        </div>
 
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-2 md:gap-10">
           {isLogine && (
-            <Link to="/orders" className="flex items-center">
-              <MdVerified className="text-4xl text-gray-800" />
+            <Link to="/orders" className="flex items-center ml-2">
+              <MdVerified className="text-2xl md:text-4xl text-gray-800" />
               <button className="hidden md:block ml-2">Orders</button>
             </Link>
           )}
 
           {isLogine ? (
             <Link to="/cart" className="relative flex items-center">
-              <FaCartPlus className="text-4xl text-gray-800" />
+              <FaCartPlus className="text-2xl md:text-4xl text-gray-800" />
               {cartCount > 0 && (
                 <span className="absolute -top-2 -right-2 rounded-full bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center">
                   {cartCount}
@@ -89,20 +100,21 @@ const Navbar = () => {
               )}
             </Link>
           ) : (
-            <Link to="/signin" className="flex items-center">
-              <FaCartPlus className="text-4xl text-gray-800" />
-            </Link>
+            
+              <FaCartPlus onClick={()=>toast.warning('Please Login')}
+                className="text-2xl md:text-4xl text-gray-800" />
+           
           )}
 
           {isLogine ? (
-            <div className="flex items-center gap-10">
+            <div className="flex items-center gap-2 md:gap-10">
               <div onClick={handleLogout} className="flex items-center">
-                <MdLogout className="text-4xl text-gray-800 cursor-pointer" />
+                <MdLogout className="text-2xl md:text-4xl text-gray-800 cursor-pointer" />
                 <button className="hidden md:block ml-2">LogOut</button>
               </div>
 
               <button
-                className="rounded-full bg-blue-950 w-[40px] h-[40px] text-white text-xl"
+                className="rounded-full bg-blue-950 w-[25px] md:w-[40px] h-[25px] md:h-[40px] text-white md:text-xl"
                 onClick={handleDrop}
               >
                 {users.name ? users.name.trim().toUpperCase()[0] : ""}
@@ -110,20 +122,32 @@ const Navbar = () => {
             </div>
           ) : (
             <Link to="/signin" className="flex items-center">
-              <MdAccountCircle className="text-4xl text-gray-800" />
+              <MdAccountCircle className="text-2xl md:text-4xl text-gray-800" />
               <button className="hidden md:block ml-2">LogIn</button>
             </Link>
           )}
         </div>
       </div>
-      {isDrop && (
-        <div className="bg-gray-400 fixed right-5 w-[250px] h-[300px] top-20 rounded-md flex flex-col items-center justify-around">
-          
-          <div className="rounded-full bg-blue-950 w-[60px] h-[60px] flex items-center justify-center text-white">
+      {isDrop && isLogine &&(
+        <div className="bg-gray-400 fixed right-5 w-[250px] h-[250px] top-20 rounded-md flex flex-col items-center justify-around shadow-lg p-4 z-50">
+          <div className="rounded-full bg-blue-950 w-[60px] h-[60px] flex items-center justify-center text-white text-2xl shadow-md">
             {users.name ? users.name.trim().toUpperCase()[0] : ""}
           </div>
-          <div className="text-xl font-sans font-semibold">Hi, {users.name}</div>
-          <div className="text-xl font-sans font-semibold">{users.email}</div>
+
+          <div className="flex flex-col items-center space-y-1 pt-3">
+            <div className="text-xl font-sans font-semibold text-gray-900">
+              Hi, {users.name}
+            </div>
+            <div className="text-lg font-sans font-medium text-gray-800">
+              {users.email}
+            </div>
+          </div>
+          <div>
+            {users.admin===true&&(
+              <button onClick={()=>navigate(`/admin/users`)}
+                className="bg-blue-600 text-white rounded-lg p-2 text-lg">Admin Page</button>
+            )}
+          </div>
         </div>
       )}
       <div>
